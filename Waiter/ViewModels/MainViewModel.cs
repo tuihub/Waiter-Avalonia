4d465a20882f496f8519c589c4ce85799483f0e3
Tabing010102 <tabing010102@outlook.com>
2023-11-10 10:54:06 +0800

    chore: add some MenuItems

--- Waiter/ViewModels/MainViewModel.cs
+++ Waiter/ViewModels/MainViewModel.cs
@@ -1,18 +1,38 @@
 ﻿using Avalonia.Controls;
 using FluentAvalonia.UI.Controls;
 using System;
+using System.Collections.Generic;
 using Waiter.Pages;
 
 namespace Waiter.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
+    public NavigationFactory NavigationFactory { get; }
+
     public MainViewModel()
     {
         NavigationFactory = new NavigationFactory(this);
     }
 
-    public NavigationFactory NavigationFactory { get; }
+    public IList<NavigationViewItem> MenuItems { get; } = new List<NavigationViewItem>
+    {
+        new NavigationViewItem
+        {
+            Content = "主界面",
+            Tag = typeof(HomePageViewModel),
+            IconSource = new SymbolIconSource { Symbol = Symbol.Home }
+        }
+    };
+    public IList<NavigationViewItem> FooterMenuItems { get; } = new List<NavigationViewItem>
+    {
+        new NavigationViewItem
+        {
+            Content = "设置",
+            Tag = typeof(SettingsPageViewModel),
+            IconSource = new SymbolIconSource { Symbol = Symbol.Setting }
+        }
+    };
 }
 
 public class NavigationFactory : INavigationPageFactory
